ddf763aa9a25aae5fb0b4e883b664936fbfbdf25
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-04-23 11:26:47 +0900

    elasticsearch service, controller 임시로 구현, 현재 insert는 진행되는데 message 값이 정상적으로 들어가지 않음

--- src/main/java/com/project/thejapenproject/controller/SearchController.java
+++ src/main/java/com/project/thejapenproject/controller/SearchController.java
@@ -4,23 +4,40 @@ package com.project.thejapenproject.controller;
 import com.project.thejapenproject.command.ResponseData;
 import com.project.thejapenproject.common.annotation.NoneAuth;
 import com.project.thejapenproject.common.annotation.NoneCheckToken;
+import com.project.thejapenproject.common.elasticsearch.TestSearchService;
+import com.project.thejapenproject.common.elasticsearch.TestVO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/search")
+@RequiredArgsConstructor
 public class SearchController {
+
+
+    private final TestSearchService testSearchService;
 
     @NoneAuth
     @GetMapping("/all")
-    public Object searchAll(@PathParam("keyWord") String keyWord){
+    public Object searchAll(@PathParam("keyWord") String keyWord) {
         System.out.println(keyWord);
+        return ResponseData.builder()
+                .code(HttpStatus.OK.value())
+                .message("성공")
+                .build();
+    }
+
+    @NoneAuth
+    @PostMapping("/add")
+    public Object searchAdd(@RequestBody Map<String, String> map) {
+        testSearchService.save(TestVO.builder()
+                .name(map.get("name"))
+                .message(map.get("massage"))
+                .build());
         return ResponseData.builder()
                 .code(HttpStatus.OK.value())
                 .message("성공")
