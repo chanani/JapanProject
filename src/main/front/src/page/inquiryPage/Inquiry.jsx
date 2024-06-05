915a0f4959c880f21168a15bfb9dec0b67e6e325
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-06-05 22:47:42 +0900

    feat : 문의사항 페이지 레이아웃, title 설정

--- src/main/front/src/page/inquiryPage/Inquiry.jsx
+++ src/main/front/src/page/inquiryPage/Inquiry.jsx
@@ -2,8 +2,28 @@ import "../../styles/inquiry/Inquiry.css";
 
 const Inquiry = () => {
     return (
-        <div>
-            문의하기
+        <div className="inquiry-container">
+            <div className="inquiry-box">
+
+                <div className="inquiry-title-box">
+                    <h2>문의하기</h2>
+                </div>
+
+                <div className="inquiry-content-box">
+
+                    <div className="inquiry-content-title">
+                        <p>번호</p>
+                        <p>분류</p>
+                        <p>제목</p>
+                        <p>글쓴이</p>
+                        <p>등록일</p>
+                    </div>
+
+                    <div className="inquiry-content-detail">
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
