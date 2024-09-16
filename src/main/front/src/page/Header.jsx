f31025982c9ee70018b3333af5bcb66e156f58ef
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-09-16 21:22:32 +0900

    chore : header 반응형 디자인(마이페이지, 로그인 및 로그이웃 시) 수정

--- src/main/front/src/page/Header.jsx
+++ src/main/front/src/page/Header.jsx
@@ -23,18 +23,19 @@ function Header() {
     const [alarmOpen, setAlarmOpen] = useState(false);
     const [noCheckList, setNoCheckList] = useState([]); // 확인하지 않은 알람 목록
     const [openSide, setOpenSide] = useState(false); // 모바일 사이드바
-    const [openCategory, setOpenCategory] = useState(Array(4).fill(false)); // 각 카테고리의 상태
+    const [openCategory, setOpenCategory] = useState(Array(5).fill(false)); // 각 카테고리의 상태
 
 
     // 학습페이지 Link list
-    const studyLink = [["/study/easy", "1단계"], ["/study/middle", "2단계"], ["/study/hard", "3단계"]];
+    const studyLink = [["/study/easy", "단어학습 1단계"], ["/study/middle", "단어학습 2단계"], ["/study/hard", "단어학습 3단계"], ["/schoolPage", '단계별학습']];
     //　테스트 페이지 Link list
-    const testLink = [["/test/easy", "1단계"], ["/test/middle", "2단계"], ["/test/hard", "3단계"]];
+    const testLink = [["/test/easy", "단어 테스트 1단계"], ["/test/middle", "단어 테스트 2단계"], ["/test/hard", "단어 테스트 3단계"]];
     // 마이페이지 Link list
     const myLink = [["/mypage/favorites", "즐겨찾기 목록"], ["/mypage/record", "학습기록"], ["/mypage", "나의정보"]];
     // 그 외 페이지 Link list
-    const otherWise = [["/chatAi", "Ai학습"], ["/search", "통합검색"], ["/notice", "공지사항"], ["/inquiry", '문의사항'],
-        ["/schoolPage", '단계별학습'], ["/translator", '번역기']]
+    const otherLink = [["/chatAi", "Ai학습"], ["/search", "통합검색"], ["/translator", '번역기']];
+    // 고객지원 list
+    const customerLink = [["/notice", "공지사항"], ["/inquiry", '문의사항']];
     // admin 페이지 Link list
     const adminLink = [["/admin/addWord", "단어추가"], ["/admin/addNotice", "공지사항 전달"], ["/mypage", "나의정보"],
         ["/addInquiryComment", '문의사항'], ["/admin/addWeekWord", '단계별단어 추가']];
@@ -92,13 +93,20 @@ function Header() {
             .catch((e) => toast.error("로그아웃 중 오류가 발생하였습니다."))
 
     }
-
+    // 회원가입 페이지 핸들러
+    const handleJoin = () => {
+        navigate("/join");
+    }
+    // 홈으로 이동 핸들러
+    const handlehome = () => {
+        navigate("/");
+    }
     useEffect(() => {
         setIsVisible(true);
     }, []);
     // 알람 SSE Connection
     useEffect(() => {
-        let eventSource;
+        /*let eventSource;
         if (userRole === 'role_user') {
             eventSource = new EventSource('http://localhost:8889/notifications/subscribe/1');
             eventSource.addEventListener('alarm', async (event) => {
@@ -115,7 +123,7 @@ function Header() {
         }
         return () => {
             eventSource.close();
-        }
+        } */
     }, [userRole]);
     // 확인하지 않은 알람
     useEffect(() => {
@@ -132,83 +140,156 @@ function Header() {
             </div>
             <div className="left-menu">
 
-                <div className="site-name-box">
+                <div className="site-name-box" onClick={handlehome}>
                     <h2>The Japan</h2>
                     <img className="header-logo" src={Logo} alt="Logo"/>
                 </div>
                 <div className="header-category-box">
-                    <div>
-                        <p>학습<MdOutlineKeyboardArrowDown/></p>
-                        <div className="header-category-detail">
-                            <p>단어학습 1단계</p>
-                            <p>단어학습 2단계</p>
-                            <p>단어학습 3단계</p>
-                        </div>
-                    </div>
-                    <div>
-                        <p>테스트<MdOutlineKeyboardArrowDown/></p>
-                        <div className="header-category-detail">
-                            <p>단어학습 1단계</p>
-                            <p>단어학습 2단계</p>
-                            <p>단어학습 3단계</p>
-                        </div>
-                    </div>
-                    <div>
-                        <p>기타페이지<MdOutlineKeyboardArrowDown/></p>
-                        <div className="header-category-detail">
-                            <p>단어학습 1단계</p>
-                            <p>단어학습 2단계</p>
-                            <p>단어학습 3단계</p>
-                        </div>
-                    </div>
-                    <div>
-                        <p>고객지원<MdOutlineKeyboardArrowDown/></p>
-                        <div className="header-category-detail">
-                            <p>단어학습 1단계</p>
-                            <p>단어학습 2단계</p>
-                            <p>단어학습 3단계</p>
-                        </div>
-                    </div>
-
+                    {['학습', '테스트', '기타페이지', '고객지원'].map((title, index) => {
+                        let links;
+                        switch (index) {
+                            case 0:
+                                links = studyLink;
+                                break;
+                            case 1:
+                                links = testLink;
+                                break;
+                            case 2:
+                                links = otherLink;
+                                break;
+                            case 3:
+                                links = customerLink;
+                                break;
+                            default:
+                                links = [];
+                        }
+                        return (
+                            <div key={index}>
+                                <p>{title}<MdOutlineKeyboardArrowDown/></p>
+                                <div className="header-category-detail">
+                                    {links.map((item, i) => (
+                                        <Link key={i} to={item[0]} >{item[1]}</Link>
+                                    ))}
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
+
             </div>
             <div className="right-menu">
-                <div className="login-box">
-                    <button className="login-btn">로그인</button>
-                    <button className="join-btn">회원가입</button>
-                </div>
-            </div>
-            <div className="mobile-right-box">
-                <button>My</button>
-            </div>
-
-            {/* 모바일 사이즈 사이드 바 */}
-            <div className={"mobile-side-bar " + (openSide ? "show" : "")} >
-                <div className="mobile-size-bar-category">
-                    {['학습', '테스트', '기타페이지', '고객지원'].map((title, index) => (
-                        <div key={index}>
-                            <div className="mobile-size-title-box" onClick={() => handleCategoryToggle(index)}>
-                                <p>{title}</p>
-                                <MdOutlineKeyboardArrowDown/>
-                            </div>
-                            <div className={"mobile-category-detail " + (openCategory[index] ? " category-show" : "")}>
-                                <p>단어학습 1단계</p>
-                                <p>단어학습 2단계</p>
-                                <p>단어학습 3단계</p>
+                {userRole === 'none' ?
+                    <div className="login-box">
+                        <button className="login-btn" onClick={handleLogin}>로그인</button>
+                        <button className="join-btn" onClick={handleJoin}>회원가입</button>
+                    </div>
+                    :
+                    <div className="header-category-box">
+                        <div className="header-category-box">
+                            <div>
+                                <p>My</p>
+                                <div className="header-category-detail mypage-detail">
+                                    {myLink.map((item, i) => (
+                                        <Link key={i} to={item[0]}>{item[1]}</Link>
+                                    ))}
+                                    <Link onClick={handleLogout}>로그아웃</Link>
+                                </div>
                             </div>
                         </div>
-                    ))}
-                    <hr style={{borderColor : "#fffcfc82", margin : "24px 0"}}/>
+                    </div>
+                }
 
-                    <div className="mobile-login-box">
-                        <button className="mobile-login-btn">로그인</button>
-                        <button className="mobile-join-btn">회원가입</button>
+            </div>
+            {userRole === 'none' ?
+                <div className="mobile-right-box">
+                    <button onClick={handleLogin}>My</button>
+                </div>
+                :
+                <div className="header-category-box show-box">
+                    <div className="header-category-box show-box">
+                        <div>
+                            <p>My</p>
+                            <div className="header-category-detail mypage-detail mobile-detail">
+                                {myLink.map((item, i) => (
+                                    <Link key={i} to={item[0]}>{item[1]}</Link>
+                                ))}
+                                <Link onClick={handleLogout}>로그아웃</Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="mobile-size-close-box">
+            }
+
+
+            {/* 모바일 사이즈 사이드 바 */}
+            <div className={"mobile-side-bar " + (openSide ? "show" : "")}>
+                <div className="mobile-size-bar-category">
+                    {['학습', '테스트', '기타페이지', '고객지원'].map((title, index) => {
+                        let links;
+                        switch (index) {
+                            case 0:
+                                links = studyLink;
+                                break;
+                            case 1:
+                                links = testLink;
+                                break;
+                            case 2:
+                                links = otherLink;
+                                break;
+                            case 3:
+                                links = customerLink;
+                                break;
+                            default:
+                                links = [];
+                        }
+                        return (
+                            <div key={index}>
+                                <div className="mobile-size-title-box" onClick={() => handleCategoryToggle(index)}>
+                                    <p>{title}</p>
+                                    <MdOutlineKeyboardArrowDown/>
+                                </div>
+                                <div
+                                    className={"mobile-category-detail " + (openCategory[index] ? " category-show" : "")}>
+                                    {links.map((item, i) => (
+                                        <Link key={i} to={item[0]} onClick={() => setOpenSide(false)}>{item[1]}</Link>
+                                    ))}
+                                </div>
+                            </div>
+                        );
+                    })}
+                    <hr style={{borderColor: "#fffcfc82", margin: "24px 0"}}/>
+
+                    {userRole === 'none' ?
+                        <div className="mobile-login-box">
+                            <button className="mobile-login-btn" onClick={handleLogin}>로그인</button>
+                            <button className="mobile-join-btn" onClick={handleJoin}>회원가입</button>
+                        </div> :
+                        <div>
+                            <div className="mobile-size-title-box" onClick={() => handleCategoryToggle(5)}>
+                                <p>마이페이지</p>
+                                <MdOutlineKeyboardArrowDown/>
+                            </div>
+                            <div
+                                className={"mobile-category-detail " + (openCategory[5] ? " category-show" : "")}>
+                                {myLink.map((item, i) => (
+                                    <Link key={i} to={item[0]}>{item[1]}</Link>
+                                ))}
+                            </div>
+                            <div className="mobile-login-box" style={{marginTop: "15px"}}>
+                                <button className="mobile-join-btn" onClick={handleLogout}>로그아웃</button>
+                            </div>
+                        </div>
+
+
+                    }
+
+                </div>
+                <div className="mobile-size-close-box" onClick={() => setOpenSide(false)}>
                     <IoMdClose size={30} fill="white" onClick={() => setOpenSide(false)}/>
                 </div>
+
             </div>
+
         </header>
     );
 }
