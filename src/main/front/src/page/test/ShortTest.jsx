c89a0b6341def069d83012e90f0e13f62722c2a5
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-11-03 14:49:39 +0900

    feat : 단답형 테스트 정답 사이드바 스크롤 반응형 제작 완료

--- src/main/front/src/page/test/ShortTest.jsx
+++ src/main/front/src/page/test/ShortTest.jsx
@@ -17,7 +17,7 @@ const ShortTest = () => {
     const [submitState, setSubmitState] = useState(false);
     // 1 = 정답, 2 = 오답, 3 = 모르겠음
     const [answerList, setAnswerList] = useState(new Array(10).fill(0)); // 정답 체크 목록 (기본값 0)
-    const [answerInput, setAnswerInput] = useState(new Array(30).fill("")); // 입력한의 답 목록
+    const [answerInput, setAnswerInput] = useState(new Array(20).fill("")); // 입력한의 답 목록
     const [word, setWord] = useState([]); // 조회한 단어 목록
     const contentText = ["너무 잘하셨어요! 학습한 보람이 있네요!", "걱정하지 마세요, 아직 배우고 있잖아요!"];
     const [sideBar, setSideBar] = useState(true); // 사이드 바 여부
