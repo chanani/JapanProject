d7310703b19b105be530fd51214eed04527c82f9
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-11-29 22:58:53 +0900

    chore : 해더 버튼에 커서 추가

--- src/main/front/src/page/Header.jsx
+++ src/main/front/src/page/Header.jsx
@@ -78,6 +78,7 @@ function Header() {
     const handleLogin = () => {
         navigate("/login");
     }
+
     // 로그아웃 핸들러
     const handleLogout = () => {
         axiosInstance.post('logout', {username: username})
