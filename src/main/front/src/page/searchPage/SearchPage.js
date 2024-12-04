b1a1bfda3f00337dc0323bbfae3d3ba82342fd9d
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-12-04 15:20:46 +0900

    refactor : 사용하지 않는 컴포넌트 삭제 및 변수 삭제

--- src/main/front/src/page/searchPage/SearchPage.js
+++ src/main/front/src/page/searchPage/SearchPage.js
@@ -2,60 +2,44 @@ import {useContext, useEffect, useState} from "react";
 import "../../styles/search/SearchPage.css"
 import axios from "axios";
 import {FaSearch} from "react-icons/fa";
-import moment from "moment";
 import 'moment/locale/ko';
 import {toast} from "react-toastify";
-import NoticeDetailNotUse from "../../component/NoticeDetailNotUse";
 import {axiosInstance} from "../../api";
 import {tokenInfoContext} from "../../component/TokenInfoProvider";
 
 const Search = () => {
-    // 키워드 저정하는 핸들러
-    const kindChangeHandle = (event) => {
-        setWordKeyword("");
-    }
+    const {username, userRole} = useContext(tokenInfoContext);
 
-    // 전체검색 리스트, 검색 요청 문구
-    const [allKeyword, setAllKeyword] = useState("");
     // 단어검색 리스트, 검색 요청 단어
     const [wordList, setWordList] = useState([]);
     const [wordKeyword, setWordKeyword] = useState("");
-    // 전체 단어 변경 핸들러
-    const allKeywordChange = (event) => {
-        setAllKeyword(event.target.value);
-        setWordKeyword(event.target.value);
-    }
+
     // 검색 단어 변경 핸들러
     const keywordChange = (event) => {
         setWordKeyword(event.target.value);
     }
 
+    // 검색된 단어 즐겨 찾기에 저장 핸들러
+    const favoriteHandle = (wordNum) => {
+        if (userRole === 'none') return toast.error("로그인 후 이용해주세요.");
+        if (!window.confirm('즐겨찾기에 추가하시겠습니까?')) return;
+        axiosInstance.post('mypage/search-register-word', {
+            username: username,
+            wordNum: wordNum
+        })
+            .then((res) => {
+                if (res.status !== 200) return toast.error('이미 추가된 단어입니다.');
+                toast.success('즐겨찾기에 추가되었습니다.');
+            })
+            .catch((e) => toast.error('즐겨찾기 추가 중 오류가 발생하였습니다.'))
+    }
+
     // 검색 버튼 핸들러
     const submitHandle = (event) => {
         if (event.key !== 'Enter') return;
-            requestWordData();
-    }
-    // 공지사항, 단어 모두 검색하는 핸들러 당장 사용하지 않을 예정
-    const doubleRequest = async () => {
-        await axios.get(`${process.env.REACT_APP_URL_ELASTICSEARCH}notice,word/_search`, {
-            params: {
-                size: 10000,
-                q: `word_meaning:${allKeyword} OR word_content:${allKeyword} OR notice_title:${allKeyword} OR notice_content:${allKeyword}`
-            },
-            auth: {
-                username: 'elastic',
-                password: 'thejapan'
-            }
-        })
-            .then((res) => {
-                console.log(res);
-            })
-    }
-    // elasticsearch로 all 데이터 요청
-    const requestAllData = () => {
-        if (!!!allKeyword) return toast.error("검색어를 입력해주세요.");
         requestWordData();
     }
+
 
     // elasticsearch로 word 데이터 요청
     const requestWordData = async () => {
@@ -80,25 +64,37 @@ const Search = () => {
     }
 
 
-
     return (
         <div className="search-box-all">
             <div className="search-box">
 
-
-
                 <div className="search-header-center">
+                </div>
+                <div className="search-box-title">
+                    <p>단어 검색</p>
 
+                    <div className="search-input-box">
+                        <input type="text" value={wordKeyword} onChange={keywordChange} onKeyDown={submitHandle}/>
+                        <FaSearch onClick={requestWordData}/>
+                    </div>
                 </div>
 
-
-                        <WordComponent
-                            wordKeyword={wordKeyword}
-                            keywordChange={keywordChange}
-                            submitHandle={submitHandle}
-                            requestWordData={requestWordData}
-                            wordList={wordList}
-                        />
+                <div className="search-content-box">
+                    {!!!wordList.length ?
+                        <div className="search-notice-content-box">
+                            <p>검색된 항목이 없습니다.</p>
+                        </div> :
+                        wordList.map((item, index) => (
+                            <div className="search-word-all" key={index}
+                                 onClick={() => favoriteHandle(item._id)}>
+                                <div className="search-word-box">
+                                    <div className="search-word-content"><h3>{item._source.word_content}</h3></div>
+                                    <div className="search-word-meaning"><p>{item._source.word_meaning}</p></div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
 
             </div>
 
@@ -109,185 +105,4 @@ const Search = () => {
 export default Search;
 
 
-const WordComponent = ({wordKeyword, keywordChange, submitHandle, requestWordData, wordList, searchKind}) => {
-    const {username} = useContext(tokenInfoContext);
-    const favoriteHandle = (wordNum) => {
-        if(!window.confirm('즐겨찾기에 추가하시겠습니까?')) return;
-        axiosInstance.post('mypage/search-register-word', {
-            username : username,
-            wordNum : wordNum
-        })
-            .then((res) => {
-                if(res.status !== 200) return toast.error('이미 추가된 단어입니다.');
-                toast.success('즐겨찾기에 추가되었습니다.');
-            })
-            .catch((e) => toast.error('즐겨찾기 추가 중 오류가 발생하였습니다.'))
-    }
-
-    return (
-        <div style={{marginBottom: "15px"}}>
-            {searchKind === "전체" ?
-                <div>
-                    <div className="search-box-mainPage-title">
-                        <p>단어 검색({wordList.length})</p>
-                    </div>
-
-                    <div className="search-content-box">
-                        {!!!wordList.length ?
-                            <div className="search-notice-content-box">
-                                <p>검색된 항목이 없습니다.</p>
-                            </div> :
-                            wordList.slice(0, 6).map((item, index) => (
-                                <div className="search-word-all" key={index}>
-                                    <div className="search-word-box">
-                                        <div className="search-word-content"><h3>{item._source.word_content}</h3></div>
-                                        <div className="search-word-meaning"><p>{item._source.word_meaning}</p></div>
-                                    </div>
-                                </div>
-                            ))
-                        }
-                    </div>
-
-                </div>
-                :
-                <div>
-                    <div className="search-box-title">
-                        <p>단어 검색</p>
-
-                        <div className="search-input-box">
-                            <input type="text" value={wordKeyword} onChange={keywordChange} onKeyDown={submitHandle}/>
-                            <FaSearch onClick={requestWordData}/>
-                        </div>
-                    </div>
-
-                    <div className="search-content-box">
-                        {!!!wordList.length ?
-                            <div className="search-notice-content-box">
-                                <p>검색된 항목이 없습니다.</p>
-                            </div> :
-                            wordList.map((item, index) => (
-                                <div className="search-word-all" key={index}
-                                onClick={() => favoriteHandle(item._id)}>
-                                    <div className="search-word-box">
-                                        <div className="search-word-content"><h3>{item._source.word_content}</h3></div>
-                                        <div className="search-word-meaning"><p>{item._source.word_meaning}</p></div>
-                                    </div>
-                                </div>
-                            ))
-                        }
-                    </div>
-                </div>
-            }
-
-        </div>
-    )
-}
-
-const NoticeComponent = ({
-                             noticeKeyword,
-                             noticeKeywordChange,
-                             submitHandle,
-                             requestNoticeData,
-                             noticeList,
-                             searchKind
-                         }) => {
-    const [detailToggle, setDetailToggle] = useState(false);
-    const [detailIndex, setDetailIndex] = useState(0);
-
-    // 제목 글자 초과할 경우 ...으로 변경
-    const truncate = (str, n) => {
-        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-    };
-    // 날짜가 오늘로부터 1일 이내인지 확인하는 함수
-    const isWithinOneDay = (date) => {
-        const oneDayAgo = moment().subtract(1, 'day');
-        return moment(date).isAfter(oneDayAgo);
-    };
-
-    // 상세 공지사항 핸들러
-    const handleDetail = ({index}) => {
-        setDetailIndex(index);
-        setDetailToggle((current) => !current);
-    }
-
-    // 상세 공지사항 닫기 핸들러
-    const handleDetailOut = () => {
-        setDetailToggle((current) => !current);
-    }
-
-    return (
-        <div style={{marginBottom: "15px"}}>
-            {searchKind === "전체" ?
-                <div>
-                    <div className="search-box-mainPage-title">
-                        <p>공지사항 검색({noticeList.length})</p>
-                    </div>
-
-                    <div className="search-notice-content-box">
-                        {!!!noticeList.length ? <p>검색된 항목이 없습니다.</p> :
-                            noticeList.slice(0, 4).map((item, index) => (
-                                <div className="user-notice-content-box" key={index}
-                                     onClick={(e) => handleDetail({index})}>
-                                    <p className="content-box-p-tag">
-                                        {truncate(item._source.notice_title, 14)}
-                                        {isWithinOneDay(item._source.notice_regdate)}
-                                    </p>
-                                    <p>{moment(item._source.notice_regdate).format('YYYY/MM/DD')}</p>
-                                </div>
-                            ))
-                        }
-                    </div>
-
-                    {detailToggle &&
-                        <NoticeDetailNotUse
-                            setDetailToggle={setDetailToggle}
-                            detailIndex={detailIndex}
-                            notice={noticeList[detailIndex]._source}
-                            handleDetailOut={handleDetailOut}
-                            kind="searchPage"
-                        />
-                    }
-
-                </div>
-                :
-                <div>
-                    <div className="search-box-title">
-                        <p>공지사항 검색</p>
-
-                        <div className="search-input-box">
-                            <input type="text" value={noticeKeyword} onChange={noticeKeywordChange}
-                                   onKeyDown={submitHandle}/>
-                            <FaSearch onClick={requestNoticeData}/>
-                        </div>
-                    </div>
-
-                    <div className="search-notice-content-box">
-                        {!!!noticeList.length ? <p>검색된 항목이 없습니다.</p> :
-                            noticeList.map((item, index) => (
-                                <div className="user-notice-content-box" key={index}
-                                     onClick={(e) => handleDetail({index})}>
-                                    <p className="content-box-p-tag">
-                                        {truncate(item._source.notice_title, 14)}
-                                        {isWithinOneDay(item._source.notice_regdate)}
-                                    </p>
-                                    <p>{moment(item._source.notice_regdate).format('YYYY/MM/DD')}</p>
-                                </div>
-                            ))
-                        }
-                    </div>
-
-                    { detailToggle &&
-                        <NoticeDetailNotUse
-                            setDetailToggle={setDetailToggle}
-                            notice={noticeList[detailIndex]._source}
-                            handleDetailOut={handleDetailOut}
-                            kind="searchPage"
-                        />
-                    }
-                </div>
-            }
-
-        </div>
-    )
-}
 
