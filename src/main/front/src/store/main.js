77fdb7763ecf5f647b79305384e1a40fc3d199b1
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-06-15 13:13:31 +0900

    chore : front 삭제

--- src/main/front/src/store/main.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import dialogSlice from "../store/modules/dialogSlice.js";
-
-// store 연결해줘야됨
-export default configureStore({
-  reducer: {
-    dialogSlice: dialogSlice.reducer,
-  },
-});
--- src/main/front/src/util/cache-key.js
+++
@@ -1,8 +0,0 @@
-const SYSTEM_NAME = "theJapan";
-class CacheKey {
-  static REFRESH_TOKEN = `${SYSTEM_NAME}-refreshToken-key`;
-  static ACCESS_TOKEN = `${SYSTEM_NAME}-accessToken-key`;
-  static USER_INFO = `${SYSTEM_NAME}-userInfo-key`;
-}
-
-export default CacheKey;
